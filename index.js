eb9dab25db0b0dd1213fee04d272240ecfb01bbf
atracts <pradhans@edgehill.ac.uk>
2024-10-28 10:57:44 +0000

    update

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function displaySidebar(){
     const sb = document.querySelector(".sidebar");
     sb.style.display = "flex";
@@ -16,23 +15,4 @@ window.addEventListener('resize', function(){
         const sb = document.querySelector(".sidebar");
         sb.style.display = "none";
     }
-=======
-function displaySidebar(){
-    const sb = document.querySelector(".sidebar");
-    sb.style.display = "flex";
-}
-
-function hideSidebar(){
-    const sb = document.querySelector(".sidebar");
-    sb.style.display = "none";
-
-}
-
-window.addEventListener('resize', function(){
-    const home = document.querySelector(".smallScreeHide")
-    if(home.offsetHeight > 0){
-        const sb = document.querySelector(".sidebar");
-        sb.style.display = "none";
-    }
->>>>>>> 8716f48c6518249b706e0a5a8badb43423ba636b
 }, true);
